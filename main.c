b13839a0f06f9473c388d76b27fc0dcf3478651e
Alberto Diaz lopez <adiaz-lo@c3r14s5.42madrid.com>
2019-10-24 11:38:23 +0200

    Main modified for testing the ft_atoi().

--- main.c
+++ main.c
@@ -1,43 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int ft_strlen(char *str);
+int	ft_atoi(char *str);
 
-void	run_ex00()
+void	run_ex03()
 {
-	char *str = "Ola k ase";
-	printf("%d", ft_strlen(str));
+	char *str = "     +--+---+--+--+124ab545";
+	printf("%d", ft_atoi(str));
 }
-
-/*void	run_ex01()
-{
-	printf("%d", ft_strncmp("aplay", "aolaz", 5));
-}*/
-
-/*void	run_ex02()
-{
-	char src[] = "1234";
-	char dest[] = "Jorge";
-	printf("%s", ft_strcat(dest, src));
-}*/
-
-/*void	run_ex03()
-{
-	char src[] = "1234";
-	char dest[] = "J";
-	printf("%s", ft_strncat(dest, src, 2));
-}*/
-
-/*void	run_ex04()
-{
-	char *str = "Foo Bar Baz";
-	char *to_find = "Bar";
-	printf("%s\n", ft_strstr(str, to_find));
-	printf("%s", strstr(str, to_find));
-}*/
 
 int main(void)
 {
-	run_ex00();
+	run_ex03();
 	return (0);
 }
