e1bee39710e2a95c035d23405cfb39c940d9551e
Alberto Diaz lopez <adiaz-lo@c1r17s3.42madrid.com>
2019-10-24 12:28:18 +0200

    Fixed the ASCII value to 48 from 50.

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,18 +6,11 @@
 /*   By: adiaz-lo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 10:59:23 by adiaz-lo          #+#    #+#             */
-/*   Updated: 2019/10/24 10:33:49 by adiaz-lo         ###   ########.fr       */
+/*   Updated: 2019/10/24 12:27:39 by adiaz-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
 	int i;
 	int counter;
@@ -26,19 +19,20 @@ int ft_atoi(char *str)
 	i = 0;
 	counter = 1;
 	result = 0;
-	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f'
+			|| str[i] == '\r' || str[i] == ' ')
 	{
 		i++;
 	}
 	while (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
-			counter*= -1;
+			counter *= -1;
 		i++;
 	}
 	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
 	{
-		result = result * 10 + (str[i] - 50);
+		result = result * 10 + (str[i] - 48);
 		i++;
 	}
 	return (result * counter);
