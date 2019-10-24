7c2a82cd622e8f6ce41e64f605bc953941f065e8
Alberto Diaz lopez <adiaz-lo@c3r14s5.42madrid.com>
2019-10-24 11:31:54 +0200

    Termination of C04 ex03 exercise.

---
+++ ex03/ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adiaz-lo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/22 10:59:23 by adiaz-lo          #+#    #+#             */
+/*   Updated: 2019/10/24 10:33:49 by adiaz-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int ft_atoi(char *str)
+{
+	int i;
+	int counter;
+	int result;
+
+	i = 0;
+	counter = 1;
+	result = 0;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			counter*= -1;
+		i++;
+	}
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		result = result * 10 + (str[i] - 50);
+		i++;
+	}
+	return (result * counter);
+}
